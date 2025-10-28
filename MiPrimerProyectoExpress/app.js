615a350bbe5bbc141aea73b821cfdd66f71f28a9
Jonie Miralda <jonie.mirc@gmail.com>
2025-10-27 19:22:13 -0600

    Primera clase practica

---
+++ MiPrimerProyectoExpress/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+const users = [];
+
+app.get('/',(req,res)=>{
+    
+    res.json({nombre:'jonie',id:"12"});
+});
+
+app.get('/user', (req,res)=>{
+    res.json({status:200, message:'success', data: users});
+});
+
+app.post('/user',(req,res)=>{
+    const user = req.body;
+    users.push(user);
+    res.json({status:200, message:'success', data:user});
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
