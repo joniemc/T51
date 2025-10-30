4aa6b0f5ad574b89aecd6a8aeafbeba82a985bb2
Jonie Miralda <jonie.mirc@gmail.com>
2025-10-29 19:18:58 -0600

    Clase miercoles 29 de octubre 2025

--- MiPrimerProyectoExpress/app.js
+++ MiPrimerProyectoExpress/app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 app.use(express.json());
 
-const users = [];
+let users = [];
 
 app.get('/',(req,res)=>{
     
@@ -24,6 +24,40 @@ app.post('/user',(req,res)=>{
     const user = req.body;
     users.push(user);
     res.json({status:200, message:'success', data:user});
+});
+
+app.put('/user',(req, res)=>{
+    const user = req.body;
+    let exists = false;
+
+    users.forEach(cuser => {
+        if(cuser.id === user.id){
+            exists = true;
+            cuser.nombre = user.nombre;
+            cuser.email = user.email;
+        }
+    });
+
+    if(exists){
+        res.json({status:200, message:'success', data:user});
+    }else{
+        res.status(404).json({status:404, message:'Usuario no encontrado...'});
+    }
+    
+});
+
+app.delete('/user/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+
+    const filtroUsuarios = users.filter(user => user.id !== id);
+
+    if(filtroUsuarios.length !== users.length){
+        users = filtroUsuarios;
+        res.json({status:200, message:'Usuario eliminado exitosamente'});
+    }
+    else{
+        res.status(404).json({status:404, message:'Usuario no encontrado...'});
+    }
 });
 
 app.listen(PORT, ()=>{
