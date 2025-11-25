b23dd1c1b2f677f27c66129edf1a24439b0fdc56
Jonie Miralda <jonie.mirc@gmail.com>
2025-11-24 18:59:05 -0600

    Tema base de datos completo

--- FabricantesConBD/app.js
+++ FabricantesConBD/app.js
@@ -57,20 +57,65 @@ app.get('/fabricantes/:nombre',(req,res)=>{
 
 app.post('/fabricantes',(req, res)=>{
     const fabricante = req.body;
-    if(!fabricante.Id || !fabricante.Nombre || !fabricante.Descripcion){
+    if(!fabricante.Nombre || !fabricante.Descripcion){
         res.status(400).json({status:400,message:'Los parametros Id, Nombre y Descripción son requeridos.'});
     }
     else{
-        const sql = 'insert into Fabricante (Id, Nombre, Descripcion) values(?,?,?)';
-        pool.query(sql,[fabricante.Id,fabricante.Nombre,fabricante.Descripcion],(err,results)=>{
+        
+        //query con un select a la tabla fabricante por nombre, 
+        // necesitamos un count si count > 0 "El registro ya existe"
+
+        const sql = 'insert into Fabricante (Nombre, Descripcion) values(?,?)';
+        pool.query(sql,[fabricante.Nombre,fabricante.Descripcion],(err,results)=>{
             if(err){
                 res.status(500).json({status:500,message:'Error en la consulta sql...'});
             }
             else{
+                fabricante.Id = results.insertId;
                 res.status(200).json({status:200,message:'Success',data:fabricante});
             }
         });
     }
+});
+
+app.put('/fabricantes',(req,res)=>{
+    const fabricante = req.body;
+    const sql = 'update Fabricante set Nombre=?, Descripcion=? where Id=?';
+
+    pool.query(sql,[fabricante.Nombre, fabricante.Descripcion, fabricante.Id], (err, results)=>{
+        if(err){
+            res.status(500).json({status:500,message:'Error en la consulta sql...'});
+        }else if(results.affectedRows === 0){
+            res.status(404).json({status:404,message:'No se encontro el registro...'});
+        }
+        else{
+            res.status(200).json({status:200,message:'Success',data:fabricante});
+        }
+    });
+});
+
+app.delete('/fabricantes/:Id',(req, res)=>{
+    const Id = parseInt(req.params.Id);
+
+    if(!Id){
+        return res.status(400).json({status:400,message:'El Id es requerido...'});
+    }
+
+    const sql = 'delete from Fabricante where Id=?';
+
+    pool.query(sql,[Id],(err, results)=>{
+        if(err){
+            return res.status(500).json({status:500,message:'Error en la consulta sql...'});
+        }
+
+        if(results.affectedRows === 0){
+            return res.status(404).json({status:404,message:'No se encontro el registro...'});
+        }
+
+        return res.status(200).json({status:200,message:'Registro eliminado exitosamente...'});
+
+    });
+
 
 });
 
